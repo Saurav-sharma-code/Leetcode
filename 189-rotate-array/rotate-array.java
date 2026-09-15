132b03497f6cfd7b703edfe5925743fe03123669
Saurav-sharma-code <sauravsharma30105@gmail.com>
2026-09-15 14:50:53 +0530

    Time: 5 ms (24.69%) | Memory: 268.8 MB (23.57%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    void reverse(int[] arr, int l, int r) {
+        while (l < r) {
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
